c22f2fc9cd7940b4164142ae9664c864c1672201
Лилит <@LAPTOP-3EJS7JBS>
2023-07-02 10:42:33 +0300

    Добавьте файлы проекта.

---
+++ HW_tema20_2/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] nums = { 1, 2, 3, 4, 5 };
+
+Console.WriteLine($"Среднее значение элементов массива: {Average_mass(nums)}");
+
+int Average_mass(params int[] numbers)
+{
+    int result = 0;
+    int count = 0;
+    foreach (var n in numbers)
+    {
+        result += n;
+        count++;
+    }
+    return result / count;
+}
+
